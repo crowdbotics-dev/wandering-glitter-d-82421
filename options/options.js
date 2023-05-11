a06538c9e5ea18906004809e910a55e6fb5de364
Crowdbotics <engineering@crowdbotics.com>
2023-05-11 21:49:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-glitter-d-82421",
+  url: "https://wandering-glitter-d-82421.botics.co",
+  api: "https://wandering-glitter-d-82421.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
